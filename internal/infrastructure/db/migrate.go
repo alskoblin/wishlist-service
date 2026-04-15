c71a5b6fffcd70279e8d30fd37afaf1efff7b0b6
Aleksandr Skoblin <aleksandr.skoblin05@gmail.com>
2026-04-15 13:13:12 +0300

    chore: add infrastructure

---
+++ internal/infrastructure/db/migrate.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(databaseURL, migrationsPath string) error {
+	m, err := migrate.New(migrationsPath, databaseURL)
+	if err != nil {
+		return fmt.Errorf("create migrator: %w", err)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		_, _ = m.Close()
+		return fmt.Errorf("apply migrations: %w", err)
+	}
+
+	srcErr, dbErr := m.Close()
+	if srcErr != nil {
+		return fmt.Errorf("close migration source: %w", srcErr)
+	}
+	if dbErr != nil {
+		return fmt.Errorf("close migration database: %w", dbErr)
+	}
+
+	return nil
+}
